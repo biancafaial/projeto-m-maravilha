3d93648bbb1c2491a266a532089baff96fb2cefd
biancafaial <baincafaial@gmail.com>
2019-05-22 22:28:25 -0300

    Finalização da imagem usando API e inicialização do metodo POST com server

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
-const boxPerfil = document.querySelector('.maravilhosas__box')
+const boxPerfil = document.querySelector('.maravilhosas__box');
+const botao = document.getElementById('button');
+
+botao.addEventListener("click", (evento) => {
+    const
+})
 
 fetch('https://theblackwomanhistory.firebaseio.com/.json')
     .then((response) => {
@@ -6,23 +11,30 @@ fetch('https://theblackwomanhistory.firebaseio.com/.json')
     })
     .then((data) => {
         console.log(data)
-        data.content.forEach(mulher => {
+        data.content.forEach((mulher) => {
             const card = document.createElement('div');
             card.setAttribute('class', 'maravilhosas__perfil');
-            boxPerfil.appendChild(card)
+            boxPerfil.appendChild(card);
+           
+            // definir imagem
+            const img = document.createElement('img');
+            img.setAttribute('class','img-responsive');
+
+            //comparação caso a imagem seja undefined o else irá definir o png como imagem do card
+            // outro mode de fazer a comparação usando OU seria if (mulher.metadata == undefined || mulher.metadata.image == "")
+            if (mulher.metadata && mulher.metadata.image){
+                img.setAttribute('src', mulher.metadata.image.url);
+            } else{
+                img.setAttribute('src','./img/img-mulher.png');
+            }
+            card.appendChild(img);
 
             const nome = document.createElement('p');
             nome.textContent = mulher.title;
             card.appendChild(nome);
-
-            // definir imagem 
-
-            // const img = document.createElement('img');
-            // img.setAttribute('class','img-responsive');
-            // img.setAttribute('src', mulher.metadata.image )
-            // card.appendChild(img)
         })
+        
     })
     .catch((erro) => {
-        console.log(erro)
+        console.log(erro);
     })
